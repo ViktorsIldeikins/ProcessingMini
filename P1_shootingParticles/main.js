602e810a93ebc23ffa9af048bc69d17441ff8925
Viktor <V.Ildeikins@inbox.lv>
2019-07-07 23:36:19 +0300

    P1: Shooting particles
    - add particle shooting by clicking mouse

--- P1_shootingParticles/main.js
+++ P1_shootingParticles/main.js
@@ -4,7 +4,11 @@ const BOUNCE_CEOF = 0.95;
 const AMOUNT = 50;
 const GRAVITY = 0.07;
 
-let particles = [];
+let particles;
+let slingshot = {
+    start: null,
+    end: null,
+};
 
 function setup() {
     createCanvas(WIDTH, HEIGHT);
@@ -15,6 +19,7 @@ function setup() {
 
 function draw() {
     background(240);
+    handleSling();
     particles.forEach(particle => particle.move());
     particles.forEach(particle => particle.show());
 }
@@ -26,4 +31,22 @@ function createParticleFan(position, vector, angle) {
         particles.push(new Particle(position.x, position.y, particleVector.x, particleVector.y));
     }
     return particles;
+}
+
+function handleSling() {
+    if (slingshot.start) {
+        slingshot.end = createVector(mouseX, mouseY);
+        stroke(200, 0, 0);
+        line(slingshot.start.x, slingshot.start.y, slingshot.end.x, slingshot.end.y);
+    }
+}
+
+function mousePressed() {
+    slingshot.start = createVector(mouseX, mouseY);
+}
+
+function mouseReleased() {
+    let shotingDirection = slingshot.start.copy().sub(slingshot.end).setMag(4);
+    particles = createParticleFan(slingshot.start.copy(), shotingDirection, toRadians(10));
+    slingshot.start = null;
 }
